c93676397fbdb3613e0e3a2e767ae6aa1e79568f
Garrik Ten <ten@otux.ru>
2023-03-09 01:00:40 +0300

    sync

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Arrays;
 import java.util.stream.Stream;
+import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.net.URL;
+import org.apache.commons.io.FileUtils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,11 +19,16 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+
 public class Main {
 
 	public static final String REMOTE_SERVICE_URI = "https://api.nasa.gov/planetary/apod?api_key=oQDtnAuzycBqHCxHprBLffwZEKm1DakJz5z3n2XE";
 
 	public static ObjectMapper mapper = new ObjectMapper();
+
+	public static void downloadFile(URL url, String fileName) throws IOException {
+		FileUtils.copyURLToFile(url, new File("fileName.jpg"));
+	}
 
 	public static void main(String[] args) throws IOException {
 		CloseableHttpClient httpClient = HttpClientBuilder.create()
@@ -31,24 +40,48 @@ public class Main {
 						.build())
 				.build();
 		// создание объекта запроса с произвольными заголовками
-		HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
-		request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+		HttpGet request1 = new HttpGet(REMOTE_SERVICE_URI);
+		request1.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
 		// отправка запроса
-		CloseableHttpResponse response = httpClient.execute(request);
-		// вывод полученных загловков
-		Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+		CloseableHttpResponse response1 = httpClient.execute(request1);
 
-		List<NasaMediaResource> nasaMediaResources = mapper.readValue(
-				response.getEntity().getContent(),
-				new TypeReference<>() {
-				});
+		// вывод полученных загловков
+		Arrays.stream(response1.getAllHeaders()).forEach(System.out::println);
+
+		String body = new String(response1.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+		NasaMediaResource nasaMediaResource = mapper.readValue(body, NasaMediaResource.class);
+
+		String url = nasaMediaResource.getUrl();
+		URL url2 = new URL(url);
+
+		downloadFile(url2, "fileName.jpg");
+
+
+		// System.out.println(url);
+
+		// System.out.println();
+
+		// HttpGet request2 = new HttpGet(url);
+		// request2.setHeader(HttpHeaders.ACCEPT,
+		// ContentType.APPLICATION_JSON.getMimeType());
+
+		// CloseableHttpResponse response2 = httpClient.execute(request2);
+
+		// Arrays.stream(response2.getAllHeaders()).forEach(System.out::println);
+
+		// System.out.println(url);
+		// List<NasaMediaResource> nasaMediaResources = mapper.readValue(
+		// response.getEntity().getContent(),
+		// new TypeReference<>() {
+		// });
 
 		// Stream<NasaMediaResource> stream = facts.stream();
 		// stream.filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
 		// .forEach(System.out::println);;
-		nasaMediaResources.forEach(System.out::println);
+		// nasaMediaResources.forEach(System.out::println);
 
-		response.close();
+		response1.close();
+		// response2.close();
 		httpClient.close();
 	}
 }
