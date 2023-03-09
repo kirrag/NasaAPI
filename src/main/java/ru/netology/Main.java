aacbc4060849fa7c344780fd92e36c9a90f60e5e
Garrik Ten <ten@otux.ru>
2023-03-10 01:05:13 +0300

    NasaAPI v1.0

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,15 +1,11 @@
 package ru.netology;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Arrays;
-import java.util.stream.Stream;
 import java.nio.charset.StandardCharsets;
 import java.io.File;
 import java.net.URL;
-import org.apache.commons.io.FileUtils;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.config.RequestConfig;
@@ -18,12 +14,13 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.commons.io.FilenameUtils;
 
 import java.awt.Dimension;
 import java.awt.image.*;
-import java.awt.event.*;
 import javax.imageio.*;
 import java.io.*;
+import javax.swing.*;
 
 public class Main {
 
@@ -31,13 +28,48 @@ public class Main {
 
 	public static ObjectMapper mapper = new ObjectMapper();
 
-	public static void downloadFile(URL url, String fileName) throws IOException {
-		FileUtils.copyURLToFile(url, new File("fileName.jpg"));
+	private static void downloadFile(String urlString, String fileName) throws IOException {
+        
+		URL url = new URL(urlString);
+              
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(url.openStream());
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        
+        byte[] buf = new byte[2048];
+        int counter = 0;
+        
+        while ((counter = bufferedInputStream.read(buf, 0, 1024)) != -1) {
+            fileOutputStream.write(buf, 0, counter);
+        }
+        
+        fileOutputStream.close();
+        bufferedInputStream.close();
+        
+        System.out.println("File Downloaded Successfully with Java Stream Operation \n");
+    }
+	public static void viewFile(String fileName) {
+
+		// Открыть сохраненный файл в буффер
+		BufferedImage img = null;
+		
+		try {
+    		img = ImageIO.read(new File(fileName));
+		} catch (IOException e) {
+			e.printStackTrace(System.out);
+		}
+		
+		// Открыть изображение из буфера
+		JLabel picLabel = new JLabel(new ImageIcon(img));
+		JPanel jPanel = new JPanel();
+		jPanel.add(picLabel);
+		JFrame f = new JFrame();
+		f.setSize(new Dimension(img.getWidth(), img.getHeight()));
+		f.add(jPanel);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
 	}
 
 	public static void main(String[] args) throws IOException {
-
-		ImageLoad appwin = new ImageLoad();
 
 		CloseableHttpClient httpClient = HttpClientBuilder.create()
 				.setUserAgent("My Test Service")
@@ -53,51 +85,21 @@ public class Main {
 		// отправка запроса
 		CloseableHttpResponse response1 = httpClient.execute(request1);
 
-		// вывод полученных загловков
-		Arrays.stream(response1.getAllHeaders()).forEach(System.out::println);
-
 		String body = new String(response1.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
 		NasaMediaResource nasaMediaResource = mapper.readValue(body, NasaMediaResource.class);
 
 		String url = nasaMediaResource.getUrl();
+		
+		// Выделение имени файла из url
+        String fileName = FilenameUtils.getName(url);
 
-		URL url2 = new URL(url);
+		// Скачать файл
+		downloadFile(url, fileName);
 
-		//java.net.Url urlObj = new java.net.Url(url2);
-		String urlPath = url2.getPath();
-		String fileName = urlPath.substring(urlPath.lastIndexOf('/') + 1);
-		System.out.println(fileName);
-
-		downloadFile(url2, "fileName.jpg");
-
-		appwin.setSize(new Dimension(400, 365));
-		appwin.setTitle("LoadImageLoad");
-		appwin.setVisible(true);
-		// System.out.println(url);
-
-		// System.out.println();
-
-		// HttpGet request2 = new HttpGet(url);
-		// request2.setHeader(HttpHeaders.ACCEPT,
-		// ContentType.APPLICATION_JSON.getMimeType());
-
-		// CloseableHttpResponse response2 = httpClient.execute(request2);
-
-		// Arrays.stream(response2.getAllHeaders()).forEach(System.out::println);
-
-		// System.out.println(url);
-		// List<NasaMediaResource> nasaMediaResources = mapper.readValue(
-		// response.getEntity().getContent(),
-		// new TypeReference<>() {
-		// });
-
-		// Stream<NasaMediaResource> stream = facts.stream();
-		// stream.filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
-		// .forEach(System.out::println);;
-		// nasaMediaResources.forEach(System.out::println);
+		// Открыть файл с изображением для просмотра
+		viewFile(fileName);
 
 		response1.close();
-		// response2.close();
 		httpClient.close();
 	}
 }
